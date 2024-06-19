5430f08fb581c5995028c248500779f99ffbeaf6
dhriti-exe <sahadhritishree@gmail.com>
2024-06-19 23:55:09 +0530

    Backend added

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import Home from './home/Home'
-import { Routes, Route } from "react-router-dom"
+import { Navigate, Routes, Route } from "react-router-dom"
 import Courses from './courses/Courses'
 import Signup from './components/Signup'
 import Contact from './contact/Contact'
-
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
 const App = () => {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
       <div className="dark:bg-slate-800 dark:text-white">
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/course" element={<Courses/>}/>
+          <Route 
+            path="/course" 
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/contactus" element={<Contact/>}/>
         </Routes>
+        <Toaster/>
       </div>
 
     </>
